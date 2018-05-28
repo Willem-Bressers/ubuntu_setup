214ce43999ca30fa05e8721d57cc9280aa83a1b7
Willem <info@willembressers.nl>
2018-05-28 11:02:26 +0200

    docker added

---
+++ setup_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+sudo apt install -y docker.io
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# sudo apt update
+# sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+# echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo apt-get update
+
+# sudo apt-get install -y docker-ce
+# sudo usermod -aG docker ${USER}
+
+# su - ${USER}
--- setup_rstudio.sh
+++ setup_rstudio.sh
@@ -2,7 +2,12 @@
 
 sudo apt update
 
-sudo apt install -y gdebi-core libjpeg62 r-base
+sudo apt install -y gdebi-core libjpeg62 r-base libssl-dev libcurl4-openssl-dev unixodbc-dev
+
+echo "r <- getOption('repos'); r['CRAN'] <- 'http://cran.us.r-project.org'; options(repos = r);" > ~/.Rprofile
+Rscript -e "install.packages('odbc')"
+Rscript -e "install.packages('packrat')"
+Rscript -e "install.packages('devtools')"
 
 wget -O $HOME/Downloads/rstudio.deb https://download1.rstudio.org/rstudio-xenial-1.1.447-amd64.deb
 
