f820df3237dbe0cb6ee1c7cf4cb4c1fb4771d044
Willem <dhr.bressers@gmail.com>
2019-10-04 16:01:22 +0200

    nodejs install (for jupyter extensions)

--- setup.sh
+++ setup.sh
@@ -8,30 +8,31 @@ WORKDIR=/media/sf_shared/projects
 # =============================================================================
 # FUNCTIONS
 # =============================================================================
-function print {
+function setup {
 	echo -e "\n\e[92m[setup]\e[0m > $1"
 }
-
 
 # =============================================================================
 # PROCESS
 # =============================================================================
-print "vim & terminator"
+
+# -----------------------------------------------------------------------------
+setup "vim & terminator"
 sudo apt install -y vim terminator
 
 if [ ! -d ${WORKDIR} ]; then
-	print "${WORKDIR}"
+	setup "${WORKDIR}"
 	mkdir ${WORKDIR}
-
 fi
 
 if [ ! -d ${HOME}/Desktop/projects ]; then
-	print "${HOME}/Desktop/projects"
+	setup "${HOME}/Desktop/projects"
 	ln -s ${WORKDIR} ${HOME}/Desktop/projects
 fi
 
 if [ ! -f ${HOME}/.bash_aliases ]; then
 	touch ${HOME}/.bash_aliases
+	echo -e "alias open=nautilus" >> ${HOME}/.bash_aliases
 fi
 
 if [ ! -f ${HOME}/.bash_profile ]; then
@@ -42,30 +43,31 @@ if [ ! -f ${HOME}/.gitignore ]; then
 	touch ${HOME}/.gitignore
 fi
 
-print "python3 & pip3"
+# -----------------------------------------------------------------------------
+setup "python3 & pip3"
 sudo apt install -y python3-pip python3-distutils
-
 if [ ! -d ${HOME}/.virtualenvs ]; then
-	print "virtualenv"
-	sudo pip3 install virtualenv virtualenvwrapper
-	echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
-	echo "export WORKON_HOME=${HOME}/.virtualenvs" >> ~/.bashrc
-	echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
-	echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+	setup "virtualenv"
+	pip3 install --user virtualenv virtualenvwrapper jupyerlabs
+	echo -e "\n# --- virtualenvwrapper ---" >> ${HOME}/.bashrc
+	echo "export WORKON_HOME=${HOME}/.virtualenvs" >> ${HOME}/.bashrc
+	echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ${HOME}/.bashrc
+	echo "source /usr/local/bin/virtualenvwrapper.sh" >> ${HOME}/.bashrc
 	sudo ln -s /etc/python3 /etc/python
 fi
 
-print "git"
+# -----------------------------------------------------------------------------
+setup "git"
 sudo apt install -y git
 
 if [ ! -f ${HOME}/.ssh/id_rsa.pub ]; then
-	print "ssh key"
+	setup "ssh key"
 	read -p "Email: (dhr.bressers@gmail.com)? " email; email=${email:-"dhr.bressers@gmail.com"}
 	ssh-keygen -t rsa -b 4096 -C $email
 	cat ${HOME}/.ssh/id_rsa.pub
 
 	if [ ! -f ${HOME}/.gitconfig ]; then
-		print "git config"
+		setup "git config"
 		read -p "Name: (Willem Bressers)? " name; name=${name:-"Willem Bressers"}
 		git config --global user.email $email
 		git config --global user.name $name
@@ -75,16 +77,17 @@ if [ ! -f ${HOME}/.ssh/id_rsa.pub ]; then
 		git config --global core.editor vim
 	fi
 
-	print "PS1"
-	echo -e "\n# GIT settings" >> ${HOME}/.bashrc
+	setup "PS1"
+	echo -e "\n# --- GIT ---" >> ${HOME}/.bashrc
 	echo -e "parse_git_branch() {
 		\tgit branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 	}" >> ${HOME}/.bashrc
 	echo "PS1=\"\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]:\[\033[33m\]\$(parse_git_branch)\[\033[00m\]$ \"" >> ${HOME}/.bashrc
 fi
 
+# -----------------------------------------------------------------------------
 if [ -z "$SETUP_SUBLIME" ]; then
-	print "sublime-text"
+	setup "sublime-text"
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	sudo apt-get install apt-transport-https
 
@@ -92,5 +95,34 @@ if [ -z "$SETUP_SUBLIME" ]; then
 
 	sudo apt update -y
 	sudo apt install -y sublime-text
+	echo -e "\n# --- SUBLIME TEXT---" >> ${HOME}/.bashrc
 	echo "export SETUP_SUBLIME=installed" >> ~/.bashrc
 fi
+
+# -----------------------------------------------------------------------------
+if [ -z "$SETUP_NODEJS" ]; then
+	VERSION=v10.16.3
+	DISTRO=linux-x64
+	
+	setup "nodejs"
+	sudo mkdir -p /usr/local/lib/nodejs
+	wget https://nodejs.org/dist/$VERSION/node-$VERSION-$DISTRO.tar.xz
+	sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs
+	rm node-$VERSION-$DISTRO.tar.xz
+	sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/node /usr/bin/node
+	sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npm /usr/bin/npm
+	sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npx /usr/bin/npx
+
+	echo "# Nodejs" >> ${HOME}/.profile
+	echo "VERSION=${VERSION}" >> ${HOME}/.profile
+	echo "DISTRO=${DISTRO}" >> ${HOME}/.profile
+	echo "export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH" >> ${HOME}/.profile
+
+	echo -e "\n# --- NODEJS ---" >> ${HOME}/.profile
+	echo "export SETUP_NODEJS=installed" >> ${HOME}/.bashrc
+
+	source ${HOME}/.profile
+	jupyter labextension install jupyterlab-python-file
+fi
+
+setup 'DONE: Run [source ~/.bashrc] now'
