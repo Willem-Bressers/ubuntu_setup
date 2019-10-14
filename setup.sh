4a457f94b2dd30d2cef2fe903adc90d323ff8048
Willem <dhr.bressers@gmail.com>
2019-10-14 11:16:11 +0200

    several update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-clear
-
 # =============================================================================
 # VARIABLES
 # =============================================================================
@@ -24,18 +22,17 @@ function message {
 if [ -z "$SETUP_GLOBAL" ]; then
 	setup "vim & terminator"
 	sudo apt install -y vim terminator
-
 	echo "export SETUP_GLOBAL=installed" >> ~/.bashrc
 fi
 
-if [ ! -d ${WORKDIR} ]; then
-	setup "${WORKDIR}"
-	mkdir ${WORKDIR}
+if [ ! -d $WORKDIR ]; then
+	setup "$WORKDIR"
+	mkdir $WORKDIR
 fi
 
-if [ ! -d ${HOME}/Desktop/projects ]; then
-	setup "${HOME}/Desktop/projects"
-	ln -s ${WORKDIR} ${HOME}/Desktop/projects
+if [ ! -d $HOME/Desktop/projects ]; then
+	setup "$HOME/Desktop/projects"
+	ln -s $WORKDIR $HOME/Desktop/projects
 fi
 
 if [ ! -f $HOME/.bash_aliases ]; then
@@ -43,56 +40,54 @@ if [ ! -f $HOME/.bash_aliases ]; then
 	echo "alias open='nautilus'" >> $HOME/.bash_aliases
 fi
 
-if [ ! -f ${HOME}/.bash_profile ]; then
-	touch ${HOME}/.bash_profile
+if [ ! -f $HOME/.bash_profile ]; then
+	touch $HOME/.bash_profile
 fi
 
-if [ ! -f ${HOME}/.gitignore ]; then
-	touch ${HOME}/.gitignore
+if [ ! -f $HOME/.gitignore ]; then
+	touch $HOME/.gitignore
 fi
 
 # -----------------------------------------------------------------------------
-if [ ! -d ${HOME}/.virtualenvs ]; then
+if [ ! -d $HOME/.virtualenvs ]; then
 	setup "python3 & pip3"
 	sudo apt install -y python3-pip python3-distutils
 
 	setup "virtualenv"
-	pip3 install --user virtualenv virtualenvwrapper jupyerlabs
-	echo -e "\n# --- virtualenvwrapper ---" >> ${HOME}/.bashrc
-	echo "export WORKON_HOME=${HOME}/.virtualenvs" >> ${HOME}/.bashrc
-	echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ${HOME}/.bashrc
-	echo "source $(which virtualenvwrapper.sh)" >> ${HOME}/.bashrc
+	pip3 install --user virtualenv virtualenvwrapper
+	echo "export WORKON_HOME=$HOME/.virtualenvs" >> $HOME/.bashrc
+	echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> $HOME/.bashrc
+	echo "source $HOME/.local/bin/virtualenvwrapper.sh" >> $HOME/.bashrc
 	# sudo ln -s /etc/python3 /etc/python
 
 	# terminator needs python to be 2.7
-	sudo ln -sf /usr/bin/python2.7 /usr/bin/python
+	sudo ln -s /usr/bin/python2.7 /usr/bin/python
 fi
 
 # -----------------------------------------------------------------------------
-if [ ! -f ${HOME}/.ssh/id_rsa.pub ]; then
+if [ ! -f $HOME/.ssh/id_rsa.pub ]; then
 	setup "git"
 	sudo apt install -y git
 
 	setup "ssh key"
 	read -p "Email: (dhr.bressers@gmail.com)? " email; email=${email:-"dhr.bressers@gmail.com"}
 	ssh-keygen -t rsa -b 4096 -C $email
-	cat ${HOME}/.ssh/id_rsa.pub
+	cat $HOME/.ssh/id_rsa.pub
 
-	if [ ! -f ${HOME}/.gitconfig ]; then
+	if [ ! -f $HOME/.gitconfig ]; then
 		setup "git config"
 		read -p "Name: (Willem Bressers)? " name; name=${name:-"Willem Bressers"}
 		git config --global user.email $email
 		git config --global user.name $name
 		git config --global push.default simple
-		git config --global core.excludesfile ${HOME}/.gitignore
+		git config --global core.excludesfile $HOME/.gitignore
 		git config --global color.ui true
 		git config --global core.editor vim
 	fi
 
 	setup "PS1"
-	echo -e "\n# --- GIT ---" >> ${HOME}/.bashrc
-	echo -e "parse_git_branch() { \n\t git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' \n}" >> ${HOME}/.bashrc
-	echo "PS1=\"\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]:\[\033[33m\]\$(parse_git_branch)\[\033[00m\]$ \"" >> ${HOME}/.bashrc
+	echo -e "parse_git_branch() { \n\t git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/' \n}" >> $HOME/.bashrc
+	echo "PS1=\"\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]:\[\033[33m\]\$(parse_git_branch)\[\033[00m\]$ \"" >> $HOME/.bashrc
 fi
 
 # -----------------------------------------------------------------------------
@@ -105,7 +100,7 @@ if [ -z "$SETUP_SUBLIME" ]; then
 
 	sudo apt update -y
 	sudo apt install -y sublime-text
-	echo -e "\n# --- SUBLIME TEXT---" >> ${HOME}/.bashrc
+	echo -e "\n# --- SUBLIME TEXT---" >> $HOME/.bashrc
 	echo "export SETUP_SUBLIME=installed" >> ~/.bashrc
 fi
 
@@ -123,15 +118,15 @@ if [ -z "$SETUP_NODEJS" ]; then
 	sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npm /usr/bin/npm
 	sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npx /usr/bin/npx
 
-	echo "# Nodejs" >> ${HOME}/.profile
-	echo "VERSION=${VERSION}" >> ${HOME}/.profile
-	echo "DISTRO=${DISTRO}" >> ${HOME}/.profile
-	echo "export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH" >> ${HOME}/.profile
+	echo "# Nodejs" >> $HOME/.profile
+	echo "VERSION=${VERSION}" >> $HOME/.profile
+	echo "DISTRO=${DISTRO}" >> $HOME/.profile
+	echo "export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH" >> $HOME/.profile
 
-	echo -e "\n# --- NODEJS ---" >> ${HOME}/.profile
-	echo "export SETUP_NODEJS=installed" >> ${HOME}/.bashrc
+	echo -e "\n# --- NODEJS ---" >> $HOME/.profile
+	echo "export SETUP_NODEJS=installed" >> $HOME/.bashrc
 
-	source ${HOME}/.profile
+	source $HOME/.profile
 	jupyter labextension install jupyterlab-python-file
 fi
 
@@ -151,22 +146,22 @@ if [ -z "$SETUP_DOCKER" ]; then
 	# post install
 	sudo usermod -aG docker $USER
 
-	echo -e "\n# --- DOCKER ---" >> ${HOME}/.profile
-	echo "export SETUP_DOCKER=installed" >> ${HOME}/.bashrc
+	echo -e "\n# --- DOCKER ---" >> $HOME/.profile
+	echo "export SETUP_DOCKER=installed" >> $HOME/.bashrc
 fi
 
 # -----------------------------------------------------------------------------
-if [ -z "$SETUP_NVIDIA_DOCKER" ]; then
-	setup "nvidia-docker"
-	distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-	curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-	curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+# if [ -z "$SETUP_NVIDIA_DOCKER" ]; then
+# 	setup "nvidia-docker"
+# 	distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+# 	curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+# 	curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
-	sudo apt-get --allow-releaseinfo-change update -y 
-	sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-	sudo systemctl restart docker
-	echo "export SETUP_NVIDIA_DOCKER=installed" >> ${HOME}/.bashrc
-fi
+# 	sudo apt-get --allow-releaseinfo-change update -y 
+# 	sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+# 	sudo systemctl restart docker
+# 	echo "export SETUP_NVIDIA_DOCKER=installed" >> $HOME/.bashrc
+# fi
 
 # -----------------------------------------------------------------------------
 echo -e ""
