8a3a5881fad81f34acb0cc1b28f675082fd4ace4
Willem <dhr.bressers@gmail.com>
2019-10-18 21:46:25 +0200

    tensorflow pet detector added

--- setup.sh
+++ setup.sh
@@ -113,9 +113,10 @@ if [ -z $SETUP_MINICONDA ]; then
 	# create some conveniant cli shortcuts
 	echo -e "\n# Conda environments" >> $HOME/.bash_aliases
 	echo -e "alias cec='conda create --name'" >> $HOME/.bash_aliases
-	echo -e "alias cer='conda env remove --name'" >> $HOME/.bash_aliases
+	echo -e "alias cer='conda deactivate && conda env remove --name'" >> $HOME/.bash_aliases
 	echo -e "alias cel='conda env list'" >> $HOME/.bash_aliases
-	echo -e "alias ce='conda activate'" >> $HOME/.bash_aliases
+	echo -e "alias cea='conda activate'" >> $HOME/.bash_aliases
+	echo -e "alias ced='conda deactivate'" >> $HOME/.bash_aliases
 	echo -e "\n# Conda packages" >> $HOME/.bash_aliases
 	echo -e "alias cip='conda install'" >> $HOME/.bash_aliases
 
