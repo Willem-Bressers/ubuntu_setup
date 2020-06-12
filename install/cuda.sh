ea64a2b205bd081306458cd5fde6b5221a13d268
Willem Bressers <dhr.bressers@gmail.com>
2020-06-12 11:00:19 +0200

    does it work?

--- install/cuda.sh
+++ install/cuda.sh
@@ -3,3 +3,14 @@
 # sudo apt install -y nvidia-cuda-toolkit
 
 # https://medium.com/@aspiring1/installing-cuda-toolkit-10-0-and-cudnn-for-deep-learning-with-tensorflow-gpu-on-ubuntu-18-04-lts-f7e968b24c98
+
+# https://illya13.github.io/RL/tutorial/2020/04/26/installing-tensorflow-on-ubuntu-20.html
+
+# switch gcc to 8
+sudo update-alternatives --config gcc
+
+# download cuda toolkit
+# wget http://developer.download.nvidia.com/compute/cuda/11.0.1/local_installers/cuda_11.0.1_450.36.06_linux.run
+
+# install the cida toolkit
+sudo bash cuda_11.0.1_450.36.06_linux.run
--- install/nvidia.sh
+++ install/nvidia.sh
@@ -1,2 +1,18 @@
+# https://illya13.github.io/RL/tutorial/2020/04/26/installing-tensorflow-on-ubuntu-20.html
+# ensure gcc >= 8
+sudo apt -y install build-essential
+sudo apt -y install gcc-8 g++-8 gcc-9 g++-9
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 8 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 9 --slave /usr/bin/g++ g++ /usr/bin/g++-9
+
+sudo update-alternatives --config gcc
+
+# download nvidia drivers at https://www.nvidia.com/drivers
+
+# install nvidia
+# cd ~/Downloads
+# sudo bash NVIDIA-Linux-x86_64-440.82.run
+
 # https://linuxconfig.org/how-to-install-the-nvidia-drivers-on-ubuntu-20-04-focal-fossa-linux
 sudo ubuntu-drivers autoinstall
